304a27a9add3c4ffd039bd770daa9226105e1357
Konstantin <antipin1987@gmail.com>
2016-05-29 13:54:40 +0300

    Added views and associations for the 13th task

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,4 @@
 class RailwayStation < ActiveRecord::Base
-  validates :title, presence: true
   has_many :trains, foreign_key: :current_station_id 
 
   #has_many :tickets, foreign_key: :first_station_id
@@ -7,4 +6,6 @@ class RailwayStation < ActiveRecord::Base
 
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
+
+  validates :title, presence: true
 end
